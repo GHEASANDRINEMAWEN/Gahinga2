ed9dfaf8516b281efa8b9a5f7f3b81c2e2ada554
Treasure <75380140+Treasure-mars@users.noreply.github.com>
2024-08-16 22:36:38 +0200

    Update user-manager.sh

--- src/Bash/user-manager.sh
+++ src/Bash/user-manager.sh
@@ -180,8 +180,6 @@ complete_registration() {
   echo "Expected demise date: $demise_date"
 }
 
-
-
 logout(){
   email=$1
 
@@ -617,24 +615,16 @@ case $1 in
     initialize_user_store
     ;;
   "get-life-expectancy")
-    if [ $# -ne 2 ]; then
-      echo "Usage: $0 get-life-expectancy <countryISO>"
-      exit 1
-    fi
     get_life_expectancy $2
     ;;
   "validate-uuid")
     if [ $# -ne 2 ]; then
-      echo "Usage: $0 validate-uuid <uuid>"
-      exit 1
+        echo "Usage: $0 validate-uuid <uuid>"
+        exit 1
     fi
     validate_uuid $2
     ;;
   "get-all-users")
-    if [ $# -ne 2 ]; then
-      echo "Usage: $0 get-all-users <status>"
-      exit 1
-    fi
     get_all_users $2
     ;;
   "initiate-registration")
@@ -645,10 +635,7 @@ case $1 in
     initiate_registration $2 $3
     ;;
   "complete-registration")
-    if [ $# -ne 11 ]; then
-      echo "Usage: $0 complete-registration <uuid> <firstName> <lastName> <dateOfBirth> <hasHIV> <diagnosisDate> <onART> <artStartDate> <countryISO> <password>"
-      exit 1
-    fi
+    shift
     complete_registration "$@"
     ;;
   "view-profile")
@@ -688,7 +675,7 @@ case $1 in
     ;;
   *)
     echo "Unknown command: $1"
-    echo "Usage: $0 <initialize-user-store|get-life-expectancy|validate-uuid|get-all-users|initiate-registration|complete-registration|view-profile|login|logout|modify-patient-profile|calculate-survival-metrics|generate-icalendar> [<args>]"
+    echo "Usage: $0 <initialize-user-store|initiate-registration|complete-registration|view-profile|login|modify-patient-profile> [<args>]"
     exit 1
     ;;
 esac
